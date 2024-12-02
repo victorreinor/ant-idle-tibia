6f01e2622c500f80ba8de506321aaa51cf129033
Victor Reinor <victorreinor@gmail.com>
2024-12-02 16:54:41 -0300

    feat: add logs with date and time, anti-idle with 14minutes

--- src/index.js
+++ src/index.js
@@ -1,34 +1,39 @@
 const { keyboard, Key, sleep } = require("@nut-tree-fork/nut-js");
+const Job = require('./Job');
+const JobManager = require('./JobManager');
 
-const Job = require('./Job')
-const JobManager = require('./JobManager')
+// Função para log com timestamp
+function logWithTimestamp(message) {
+  const timestamp = new Date().toISOString(); // Formato ISO para data e hora
+  console.log(`[${timestamp}] ${message}`);
+}
 
 // Funções específicas de ação
 async function pressSixAction() {
-  console.log("Pressionando a tecla 6");
+  logWithTimestamp("Pressionando a tecla 6");
   await keyboard.pressKey(Key.Num6);
   await sleep(500); // Aguarda 500ms
   await keyboard.releaseKey(Key.Num6);
-  console.log("Tecla 6 pressionada e liberada");
+  logWithTimestamp("Tecla 6 pressionada e liberada");
 }
 
 async function pressCtrlArrowAction() {
-  console.log("Executando Ctrl + Right Arrow");
+  logWithTimestamp("Executando Ctrl + Right Arrow");
   await keyboard.pressKey(Key.LeftControl, Key.Right);
   await sleep(500);
   await keyboard.releaseKey(Key.LeftControl, Key.Right);
 
-  console.log("Executando Ctrl + Left Arrow");
+  logWithTimestamp("Executando Ctrl + Left Arrow");
   await keyboard.pressKey(Key.LeftControl, Key.Left);
   await sleep(500);
   await keyboard.releaseKey(Key.LeftControl, Key.Left);
 
-  console.log("Rotina Ctrl + Arrow finalizada");
+  logWithTimestamp("Rotina Ctrl + Arrow finalizada");
 }
 
 // Configuração dos jobs
 const pressSixJob = new Job("Soltar exori gran: Hotkey 6 (30s)", 30 * 1000, pressSixAction);
-const pressCtrlArrowJob = new Job("Anti-idle Ctrl + Arrow (40s) ", 40 * 1000, pressCtrlArrowAction);
+const pressCtrlArrowJob = new Job("Anti-idle Ctrl + Arrow (40s)", 14 * 60 * 1000, pressCtrlArrowAction);
 
 // Gerenciador de jobs
 const jobManager = new JobManager();
